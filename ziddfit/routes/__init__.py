c71a9c5ec8de03cf85d717230f1f5d58287ff1e1
HackRogue <mazumdarh549@gmail.com>
2026-02-01 12:10:52 +0530

    Added the url routing

--- ziddfit/routes/__init__.py
+++ ziddfit/routes/__init__.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 
 urlpatterns = [
-    path('members/', include('gym_app.routes.member_routes')),
-    path('auth/', include('gym_app.routes.user_routes')),
+    path('members/', include('ziddfit.routes.member_routes')),
+    path('gyms/', include('ziddfit.routes.gym_routes')),
+    path('auth/', include('ziddfit.routes.user_routes')),
 ]
---
+++ ziddfit/routes/gym_routes.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from ..views.gym_views import gym_list, gym_detail 
+
+urlpatterns = [
+    path('/', gym_list, name='gym-list-create'),
+    path('/<gym_id>', gym_detail, name = 'gym-detail')
+]
