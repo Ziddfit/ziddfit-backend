075ab7e70f2c461dd89915ac7960e691f8918341
Kritiman2005 <kingzkritiman@gmail.com>
2026-02-03 21:20:23 +0530

    added pagination import

--- core/views/attendance_views.py
+++ core/views/attendance_views.py
@@ -11,7 +11,7 @@ from core.models.attendance import GymAttendance
 from core.models.gym import Gym
 from core.models.members import GymMember
 from core.serializers.attendance_serializer import GymAttendanceSerializer
-
+from core.pagination import StandardResultsPagination
 @api_view(['GET', 'POST'])
 def attendance_list(request, gym_id):
     if request.method == 'GET':
