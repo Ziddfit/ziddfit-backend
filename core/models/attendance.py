661b04cd85f3ba7fe98c49559413b09a093ea0eb
HackRogue <mazumdarh549@gmail.com>
2026-02-03 16:59:43 +0530

    Fixed issue-3

--- core/models/attendance.py
+++ core/models/attendance.py
@@ -9,11 +9,20 @@ class GymAttendance(models.Model):
         on_delete= models.CASCADE,
         related_name = 'gymattends'
     )
-    user = models.ForeignKey(
+    member = models.ForeignKey(
         GymMember,
         on_delete = models.CASCADE,
         related_name= 'memattends'
     )
     checkin_time = models.DateTimeField(auto_now_add= True)
     entry_source = models.CharField(max_length=50, default='QR_SCAN')
+    date = models.DateField(auto_now_add=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['gym', 'member', 'date'],
+                name='unique_daily_attendance'
+            )
+        ]
     
