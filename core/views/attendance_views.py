f733b2b21de0c3df9842b3e4e6eb878a29041eb4
HackRogue <mazumdarh549@gmail.com>
2026-02-09 21:18:05 +0530

    Added global paginator class

--- core/views/attendance_views.py
+++ core/views/attendance_views.py
@@ -13,6 +13,7 @@ from core.models.members import GymMember
 from core.serializers.attendance_serializer import GymAttendanceSerializer
 from core.pagination import StandardResultsPagination
 
+
 @api_view(['GET', 'POST'])
 def attendance_list(request, gym_id):
     if request.method == 'GET':
