1bd9ecca55a6c9f16ac005381efdd1ad55267fd1
Kritiman2005 <kingzkritiman@gmail.com>
2026-02-11 23:56:20 +0530

    imported owner

--- core/models/gym.py
+++ core/models/gym.py
@@ -2,12 +2,12 @@ from django.db import models
 from django.conf import settings
 from django.contrib.postgres.indexes import GinIndex
 import uuid
-from users.models import User
+from owners.models import Owner
 
 class Gym(models.Model):
     id = models.UUIDField(primary_key=True, default = uuid.uuid4, editable= False)
     owner = models.ForeignKey(
-        User, 
+        Owner, 
         on_delete= models.CASCADE,
         related_name= 'gyms'
     )
