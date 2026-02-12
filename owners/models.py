ea0910ca9d43c39d7f160a436716627da4163462
Kritiman2005 <kingzkritiman@gmail.com>
2026-02-13 00:18:49 +0530

    Initialised the owner model

--- owners/models.py
+++ owners/models.py
@@ -2,10 +2,16 @@ from django.db import models
 from users.models import User
 
 class Owner(User):
-
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='owner_profile'
+    )
     business_name = models.CharField(max_length= 255)
     tax_id = models.CharField(max_length=50, unique=True, null=True)
 
     class Meta:
         verbose_name = "Gym Owner"
         db_table = "owners_owner"
+    def __str__(self):
+        return self.user.email    
