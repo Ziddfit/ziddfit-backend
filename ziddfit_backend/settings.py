069224ab05fe89f2e99bb787e82d58efbb6c5644
HackRogue <mazumdarh549@gmail.com>
2026-03-03 21:07:18 +0530

    Added ps support

--- ziddfit_backend/settings.py
+++ ziddfit_backend/settings.py
@@ -80,10 +80,15 @@ AUTH_USER_MODEL = 'users.User'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# If using the dictionary:
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ziddfit-dev',  # Update this!
+        'USER': 'postgres',
+        'PASSWORD': 'your_password',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
