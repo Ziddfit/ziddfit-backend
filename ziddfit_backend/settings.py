37f2924fef25124576de088ebbaa694e0a3e57f5
HackRogue <mazumdarh549@gmail.com>
2026-03-03 20:59:04 +0530

    Changed the development server to postgres

--- ziddfit_backend/settings.py
+++ ziddfit_backend/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -83,8 +82,16 @@ AUTH_USER_MODEL = 'users.User'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ziddfit-dev',           # from dbname
+        'USER': 'postgres',           # from user
+        'PASSWORD': 'Mrinmoy100',  # from password
+        'HOST': 'localhost',          # from host
+        'PORT': '5432',               # from port
+        'OPTIONS': {
+            'connect_timeout': 10,    # from connect_timeout
+            'sslmode': 'prefer',      # from sslmode
+        }
     }
 }
 
