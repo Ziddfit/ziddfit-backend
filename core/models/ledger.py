864055a2dc849b560551f58cef18555455188378
HackRogue <mazumdarh549@gmail.com>
2026-03-19 00:01:03 +0530

    ledger

--- core/models/ledger.py
+++ core/models/ledger.py
@@ -7,6 +7,7 @@ from ..models.gym_staff import GymStaff
 
 class Transaction(models.Model):
 
+
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length= 255)
     gym = models.ForeignKey(
