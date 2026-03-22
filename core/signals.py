97af1843e9946b47e272bc978bd106e491410095
HackRogue <mazumdarh549@gmail.com>
2026-03-22 19:46:56 +0530

    Added signals for creation of default plans and all when a new plan is created

---
+++ core/signals.py
@@ -0,0 +1,33 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from core.models.gym import Gym
+from core.models.members import GymMember
+from core.models.subscription import GymSubscription
+from datetime import date, timedelta
+
+@receiver(post_save, sender = Gym)
+def create_default_plan(sender, instance, created, **kwargs):
+    if created:
+        GymSubscription.objects.create(
+            gym=instance,
+            plan_type='MONTHLY',
+            time_period=30,
+            price=0,
+            description='Default free plan',
+            is_default=True,
+            is_active=True,
+        )
+
+@receiver(post_save, sender=GymMember)
+def assign_default_plan(sender, instance, created, **kwargs):
+    if created and not instance.subscription:
+        default_plan = GymSubscription.objects.filter(
+            gym=instance.gym,
+            is_default=True,
+            is_active=True
+        ).first()
+
+        if default_plan:
+            instance.subscription = default_plan
+            instance.membership_end = date.today() + timedelta(days=default_plan.time_period)
+            instance.save(update_fields=['subscription', 'membership_end'])
